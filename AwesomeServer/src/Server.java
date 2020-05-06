7b02ee6451ce824b0343881e646d1aa8ec0fc7b3
Wiaci <vyachproz7@gmail.com>
2020-05-06 20:55:31 +0300

    qqwwweerty

--- AwesomeServer/src/Server.java
+++ AwesomeServer/src/Server.java
@@ -1,9 +1,45 @@
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
 
 public class Server {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        DatagramChannel server = DatagramChannel.open();
+        InetSocketAddress iAdd = new InetSocketAddress("localhost", 8989);
+        server.bind(iAdd);
+        System.out.println("Server Started: " + iAdd);
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        //receive buffer from client.
+        while (true) {
+            buffer.clear();
+            SocketAddress remoteAdd = server.receive(buffer);
+            //change mode of buffer
+            buffer.flip();
+            int limits = buffer.limit();
+            byte[] bytes = new byte[limits];
+            buffer.get(bytes, 0, limits);
+            String msg = new String(bytes);
+            if (msg.equals("exit")) break;
+            System.out.println("Client at " + remoteAdd + "  sent: " + msg);
+            buffer.flip();
+            server.send(buffer, remoteAdd);
+        }
+        server.close();
 
+        /*byte[] bytes = new byte[10];
+        SocketAddress address = new InetSocketAddress(8000);
+        System.out.println("Server Started\n" + address);
+        DatagramChannel channel = DatagramChannel.open();
+        channel.socket().bind(address);
+        ByteBuffer buffer = ByteBuffer.wrap(bytes);
+        buffer.clear();
+        channel.receive(buffer);
+        System.out.println("Принято");
+        for (int j = 0; j < 10; j++) {
+            bytes[j] *= 2;
+        }
+        buffer.flip();
+        channel.send(buffer, address);*/
     }
 }
--- NiceClient/src/Client.java
+++ NiceClient/src/Client.java
@@ -1,9 +1,28 @@
 import java.io.*;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        DatagramChannel client = DatagramChannel.open();
+        String msg = new Scanner(System.in).nextLine();
+        ByteBuffer buffer = ByteBuffer.wrap(msg.getBytes());
+        InetSocketAddress serverAddress = new InetSocketAddress("localhost",
+                8989);
 
+        client.send(buffer, serverAddress);
+        buffer.clear();
+        client.receive(buffer);
+        buffer.flip();
+        byte[] b = new byte[buffer.limit()];
+        buffer.get(b, 0, b.length);
+        System.out.println(new String(b));
+        client.close();
     }
 }
