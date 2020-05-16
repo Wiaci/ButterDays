87ce8f699b8d083c9fcc80e49b7cd730f0839e58
Wiaci <Eculk3Ofabmo>
2020-05-16 11:03:42 +0300

    finalOchka2

--- AwesomeServer/src/ServerMain.java
+++ AwesomeServer/src/ServerMain.java
@@ -8,15 +8,19 @@ public class ServerMain {
 
     static AwesomeServer awesomeServer;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             if (awesomeServer != null) {
                 FileSaver.save(awesomeServer.getCommandProcessor(), args[0]);
             }
         }));
-        if (args.length > 0) {
-            awesomeServer = new AwesomeServer(args[0]);
-            awesomeServer.run();
-        } else System.out.println("Нужен файл с коллекцией!");
+        try {
+            if (args.length > 0) {
+                awesomeServer = new AwesomeServer(args[0]);
+                awesomeServer.run();
+            } else System.out.println("Нужен файл с коллекцией!");
+        } catch (IOException e) {
+            System.out.println("Произошло непредвиденное нечто");
+        }
     }
 }
